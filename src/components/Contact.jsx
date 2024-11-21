a55825d365e0d29b98fb7c1e90adeb90d79de71a
Firdavs-Normurodov <firdavsnormurodov435@gmail.com>
2024-11-21 17:02:22 +0300

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -88,14 +88,14 @@ const Contact = () => {
                 </a>
               </dd>
             </div>
-            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            {/* <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
               <dt className="text-sm font-medium leading-6 text-colorT">
                 Telephone number
               </dt>
               <dd className="mt-1 transition-all text-sm leading-6 text-lightText sm:col-span-2 sm:mt-0">
                 +7(901)994-50-99
               </dd>
-            </div>
+            </div> */}
           </dl>
         </div>
       </div>
